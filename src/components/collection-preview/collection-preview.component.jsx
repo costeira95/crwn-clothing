c5208ae63c02390b5c2fbc1b942b9e1c1bbab2af
Daniel Costeira <costeiradaniel@gmail.com>
2020-03-31 23:45:31 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -9,8 +9,8 @@ import "./collection-preview.styles.scss";
           {
             items
             .filter((item, idx) => idx < 4)
-            .map(({ id, ...otherItemProps }) => (
-              <CollectionItem key={id} {...otherItemProps} />
+            .map((item) => (
+              <CollectionItem key={item.id} item={item} />
             ))
           }
       </div>
