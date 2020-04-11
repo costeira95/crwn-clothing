eba835420f77154525225cc842f2d86b373f563a
Daniel Costeira <costeiradaniel@gmail.com>
2020-04-11 15:37:33 +0100

    Converting every component from sass to styled-components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -10,35 +9,35 @@ import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import "./header.styles.scss";
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo  className="logo"/>
-    </Link>
+  <HeaderContainer>
+    <LogoContainer to="/">
+      <Logo className="logo"/>
+    </LogoContainer>
 
-    <div className="options">
-      <Link to="/shop" className="option">
+    <OptionsContainer>
+      <OptionLink to="/shop">
         SHOP
-      </Link>
+      </OptionLink>
 
-      <Link to="/contact" className="option">
+      <OptionLink to="/contact">
         CONTACT
-      </Link>
+      </OptionLink>
 
       {
         currentUser ?
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+        <OptionLink as="div" onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
         :
-        <Link className="option" to="/signin">SIGN IN</Link>
+        <OptionLink to="/signin">SIGN IN</OptionLink>
       }
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {
       hidden ? null : <CardDropdown />
     }
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
